6183885c5428e7a395068650f19e5553a772fe91
WojtekKirsz <w.kirsz@gmail.com>
2023-11-11 19:08:32 +0100

    element div

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 function generateGalleryItem(item) {
   const galleryItem = document.createElement('div');
