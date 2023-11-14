891ff27f612b2a4b1b7f96c53b7ecb8b594e7c21
WojtekKirsz <w.kirsz@gmail.com>
2023-11-14 23:05:52 +0100

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,58 @@
-const form = document.querySelector('.feedback-form');
+// const form = document.querySelector('.feedback-form');
 
-const localStorageKey = 'message';
+// const localStorageKey = 'message';
 
-form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
+// form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
 
-form.addEventListener('input', event => {
-  localStorage.setItem(localStorageKey, event.target.value);
+// form.addEventListener('input', event => {
+//   localStorage.setItem(localStorageKey, event.target.value);
+// });
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   localStorage.removeItem(localStorageKey);
+//   form.reset();
+// });
+
+// Importowanie funkcji throttle z lodash
+import throttle from 'lodash/throttle';
+
+// Funkcja do aktualizacji pól formularza na podstawie danych z local storage
+const updateFormFieldsFromStorage = () => {
+  const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedState) {
+    form.elements.email.value = savedState.email || '';
+    form.elements.message.value = savedState.message || '';
+  } else {
+    form.reset();
+  }
+};
+
+// Funkcja throttle do zapisywania danych z formularza do local storage
+const updateLocalStorageThrottled = throttle(() => {
+  const currentState = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+}, 500); // Opóźnienie zapisu na 500 milisekund
+
+// Zdarzenie input na polach formularza - wywołuje zapis do local storage
+form.addEventListener('input', () => {
+  updateLocalStorageThrottled();
 });
 
+// Po załadowaniu strony - aktualizacja pól formularza z local storage
+updateFormFieldsFromStorage();
+
+// Zdarzenie submit formularza - czyszczenie local storage i reset pola
 form.addEventListener('submit', event => {
   event.preventDefault();
-  localStorage.removeItem(localStorageKey);
+  const currentState = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  console.log('Submitted form state:', currentState);
+  localStorage.removeItem('feedback-form-state');
   form.reset();
 });
