a2b9e4da3402aeda9c964712f929f2c4be6cc123
WojtekKirsz <w.kirsz@gmail.com>
2023-11-14 22:21:23 +0100

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,41 +11,24 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-// Handler zdarzenia 'timeupdate' z wykorzystaniem _.throttle
 const throttledTimeUpdateHandler = throttle(function (data) {
   const currentTime = data.seconds;
 
-  // Zapisz aktualny czas odtwarzania w Local Storage
   saveCurrentTimeToLocalStorage(currentTime);
   console.log('Current time played:', currentTime, 'seconds');
   console.log('Duration:', data.duration, 'seconds');
   console.log('Percent played:', data.percent * 100, '%');
-}, 1000); // Czas opóźnienia w milisekundach (ustaw na 1000, aby zaktualizować co sekundę)
+}, 1000);
 
-// Dodaj handler do zdarzenia 'timeupdate'
 player.on('timeupdate', throttledTimeUpdateHandler);
 
-// player.on('timeupdate', function (data) {
-//   // Zapisz aktualny czas odtwarzania w Local Storage
-//   saveCurrentTimeToLocalStorage(data.seconds);
-
-//   console.log('Current time played:', data.seconds, 'seconds');
-//   console.log('Duration:', data.duration, 'seconds');
-//   console.log('Percent played:', data.percent * 100, '%');
-// });
-
-// Funkcja do zapisywania czasu odtwarzania w Local Storage
 function saveCurrentTimeToLocalStorage(currentTime) {
   localStorage.setItem('videoplayer-current-time', currentTime);
 }
 
-// Funkcja do pobierania czasu odtwarzania z Local Storage
 function getCurrentTimeFromLocalStorage() {
   const storedTime = localStorage.getItem('videoplayer-current-time');
 
-  // return storedTime ? parseFloat(storedTime) : 0;
-
-  // alternative code below
   if (storedTime) {
     return parseFloat(storedTime);
   } else {
@@ -53,11 +36,9 @@ function getCurrentTimeFromLocalStorage() {
   }
 }
 
-// Funkcja do wznowienia odtwarzania od zapisanego momentu
 function resumePlayback() {
   const storedTime = getCurrentTimeFromLocalStorage();
   player.setCurrentTime(storedTime);
 }
 
-// Wznowienie odtwarzania od zapisanego momentu po przeładowaniu strony
 resumePlayback();
