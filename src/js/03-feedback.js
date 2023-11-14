68a5d96ebc6ab06f5a11f848e8a46429354ab779
WojtekKirsz <w.kirsz@gmail.com>
2023-11-14 22:52:56 +0100

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.feedback-form');
 
+const localStorageKey = 'value';
+
+form.elements.message.value = localStorageKey.getItem(localStorageKey) ?? '';
+
+form.addEventListener('input', event => {
+  localStorage.setItem(localStorageKey, event.target.value);
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
