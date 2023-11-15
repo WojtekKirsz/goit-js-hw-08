72eac7e716b933d55830c978123c30803f7bcd28
WojtekKirsz <118019899+WojtekKirsz@users.noreply.github.com>
2023-11-15 21:25:53 +0000

    Deploying to gh-pages from @ WojtekKirsz/goit-js-hw-08@6ea9f50ddbeff88f4bec290a22365caec6dba3cc 🚀

--- 03-feedback.5b36c088.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t=document.querySelector(".feedback-form"),a="message";t.elements.message.value=null!==(e=localStorage.getItem(a))&&void 0!==e?e:"",t.addEventListener("input",(function(e){localStorage.setItem(a,e.target.value)})),t.addEventListener("submit",(function(e){e.preventDefault(),localStorage.removeItem(a),t.reset()}))}();
-//# sourceMappingURL=03-feedback.5b36c088.js.map
